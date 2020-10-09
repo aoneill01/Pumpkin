97e75131049877b45785aa00704c927bd09aa2e4
Andy O'Neill <aoneill01@gmail.com>
2020-10-09 11:57:34 -0400

    Include all 10 eyes

--- Pumpkin.ino
+++ Pumpkin.ino
@@ -1,31 +1,42 @@
 #include <Servo.h>
-#define MIN_POS 55
+#define MIN_POS 60
 #define MAX_POS 135
+#define NUM_EYES 10
 
-Servo eye;
+Servo eye[NUM_EYES];
 
 void moveEye(int eyeId, byte position) {
   float factor = constrain(position, 0, 255) / 255.0;
   int servoPosition = (int)(factor * (MAX_POS - MIN_POS) + MIN_POS);
-  eye.write(servoPosition);
+  eye[eyeId].write(servoPosition);
   // Serial.println(servoPosition, DEC);
+}
+
+void moveAllEyes(byte position) {
+  for (int i = 0; i < NUM_EYES; i++) {
+    moveEye(i, position);
+    delay(200);
+  }
 }
 
 void setup() {
   // Serial.begin(9600);
-  eye.attach(9);
+  for (int i = 0; i < NUM_EYES; i++) {
+    eye[i].attach(3 + i);
+    delay(200);
+  }
 
-  moveEye(0, 127);
-  delay(2000);
-  moveEye(0, 0);
-  delay(2000);
-  moveEye(0, 255);
-  delay(2000);
-  moveEye(0, 127);
-  delay(2000);
+  moveAllEyes(127);
+  delay(500);
+  moveAllEyes(0);
+  delay(500);
+  moveAllEyes(255);
+  delay(500);
+  moveAllEyes(127);
+  delay(500);
 }
 
 void loop() {
-  moveEye(0, (byte)random(255));
-  delay(1000);
+  moveEye(random(NUM_EYES), (byte)random(255));
+  delay(150);
 }
